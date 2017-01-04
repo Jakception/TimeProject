f562c85d2aab0770586c03f26716a8051ab44530
Ludivine <redxliiife@gmail.com>
2017-01-04 12:18:09 +0100

    Modif - Ajout des noms au éléments du form (bouton etc)

--- TimeProject/FormLog.Designer.cs
+++ TimeProject/FormLog.Designer.cs
@@ -28,85 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btn_valIdentification = new System.Windows.Forms.Button();
+            this.textBox_Identifiant = new System.Windows.Forms.TextBox();
+            this.textBox_pwd = new System.Windows.Forms.TextBox();
+            this.lab_Identif = new System.Windows.Forms.Label();
+            this.lab_pwdIdentif = new System.Windows.Forms.Label();
+            this.lab_TitleIdentif = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_valIdentification
             // 
-            this.button1.AccessibleName = "btn_ValIdentification";
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(181)))), ((int)(((byte)(193)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(181)))), ((int)(((byte)(193)))));
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(467, 399);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Valider";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_valIdentification.AccessibleName = "";
+            this.btn_valIdentification.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(181)))), ((int)(((byte)(193)))));
+            this.btn_valIdentification.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(181)))), ((int)(((byte)(193)))));
+            this.btn_valIdentification.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
+            this.btn_valIdentification.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
+            this.btn_valIdentification.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_valIdentification.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_valIdentification.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_valIdentification.Location = new System.Drawing.Point(467, 399);
+            this.btn_valIdentification.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_valIdentification.Name = "btn_valIdentification";
+            this.btn_valIdentification.Size = new System.Drawing.Size(112, 35);
+            this.btn_valIdentification.TabIndex = 0;
+            this.btn_valIdentification.Text = "Valider";
+            this.btn_valIdentification.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // textBox_Identifiant
             // 
-            this.textBox1.Location = new System.Drawing.Point(467, 218);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(242, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBox_Identifiant.Location = new System.Drawing.Point(467, 218);
+            this.textBox_Identifiant.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_Identifiant.Name = "textBox_Identifiant";
+            this.textBox_Identifiant.Size = new System.Drawing.Size(242, 26);
+            this.textBox_Identifiant.TabIndex = 1;
             // 
-            // textBox2
+            // textBox_pwd
             // 
-            this.textBox2.Location = new System.Drawing.Point(467, 297);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(242, 26);
-            this.textBox2.TabIndex = 2;
+            this.textBox_pwd.Location = new System.Drawing.Point(467, 297);
+            this.textBox_pwd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_pwd.Name = "textBox_pwd";
+            this.textBox_pwd.Size = new System.Drawing.Size(242, 26);
+            this.textBox_pwd.TabIndex = 2;
             // 
-            // label1
+            // lab_Identif
             // 
-            this.label1.AccessibleName = "lab_Identif";
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
-            this.label1.Location = new System.Drawing.Point(288, 218);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Identifiant";
+            this.lab_Identif.AccessibleName = "";
+            this.lab_Identif.AutoSize = true;
+            this.lab_Identif.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_Identif.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
+            this.lab_Identif.Location = new System.Drawing.Point(288, 218);
+            this.lab_Identif.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lab_Identif.Name = "lab_Identif";
+            this.lab_Identif.Size = new System.Drawing.Size(109, 24);
+            this.lab_Identif.TabIndex = 3;
+            this.lab_Identif.Text = "Identifiant";
             // 
-            // label2
+            // lab_pwdIdentif
             // 
-            this.label2.AccessibleName = "lab_pwdIdentif";
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
-            this.label2.Location = new System.Drawing.Point(288, 297);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(145, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Mot de passe";
+            this.lab_pwdIdentif.AccessibleName = "";
+            this.lab_pwdIdentif.AutoSize = true;
+            this.lab_pwdIdentif.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_pwdIdentif.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
+            this.lab_pwdIdentif.Location = new System.Drawing.Point(288, 297);
+            this.lab_pwdIdentif.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lab_pwdIdentif.Name = "lab_pwdIdentif";
+            this.lab_pwdIdentif.Size = new System.Drawing.Size(145, 24);
+            this.lab_pwdIdentif.TabIndex = 4;
+            this.lab_pwdIdentif.Text = "Mot de passe";
             // 
-            // label3
+            // lab_TitleIdentif
             // 
-            this.label3.AccessibleName = "lab_TitleIdentif";
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
-            this.label3.Location = new System.Drawing.Point(335, 49);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(323, 29);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "TimeProject - Identification";
+            this.lab_TitleIdentif.AccessibleName = "lab_TitleIdentif";
+            this.lab_TitleIdentif.AutoSize = true;
+            this.lab_TitleIdentif.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_TitleIdentif.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(90)))), ((int)(((byte)(165)))));
+            this.lab_TitleIdentif.Location = new System.Drawing.Point(335, 49);
+            this.lab_TitleIdentif.Name = "lab_TitleIdentif";
+            this.lab_TitleIdentif.Size = new System.Drawing.Size(323, 29);
+            this.lab_TitleIdentif.TabIndex = 5;
+            this.lab_TitleIdentif.Text = "TimeProject - Identification";
             // 
             // FormLog
             // 
@@ -114,12 +114,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
             this.ClientSize = new System.Drawing.Size(968, 741);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lab_TitleIdentif);
+            this.Controls.Add(this.lab_pwdIdentif);
+            this.Controls.Add(this.lab_Identif);
+            this.Controls.Add(this.textBox_pwd);
+            this.Controls.Add(this.textBox_Identifiant);
+            this.Controls.Add(this.btn_valIdentification);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormLog";
@@ -132,12 +132,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_valIdentification;
+        private System.Windows.Forms.TextBox textBox_Identifiant;
+        private System.Windows.Forms.TextBox textBox_pwd;
+        private System.Windows.Forms.Label lab_Identif;
+        private System.Windows.Forms.Label lab_pwdIdentif;
+        private System.Windows.Forms.Label lab_TitleIdentif;
     }
 }
 
