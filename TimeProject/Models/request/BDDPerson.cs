a785600594cde319fe9ac750f585d64a01c53a33
2rdev <pro2rdev@gmail.com>
2017-01-04 11:41:45 +0100

    Constructeur Personne + classe BDD Personne

---
+++ TimeProject/Models/request/BDDPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeProject.Class;
+
+namespace TimeProject.Models.request
+{
+    class BDDPerson
+    {
+        private Personne pers;
+        private List<Personne> lstPers = new List<Personne>();
+    }
+}
