f99024797d1c12431a40ff207775c9cdc9a33160
2rdev <pro2rdev@gmail.com>
2017-01-18 12:17:30 +0100

    Gestion Form Accueil

---
+++ TimeProject/FormAccueil.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TimeProject
+{
+    partial class FormAccueil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(394, 53);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(150, 45);
+            this.lblTitre.TabIndex = 0;
+            this.lblTitre.Text = "ACCUEIL";
+            // 
+            // FormAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(170)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(923, 629);
+            this.Controls.Add(this.lblTitre);
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Name = "FormAccueil";
+            this.Text = "FormAccueil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitre;
+    }
+}
