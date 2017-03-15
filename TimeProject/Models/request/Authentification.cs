4c0e0a067712b02d4cf0ce0c4cf877eb7de561f3
Jakception <gbeulaygue@gmail.com>
2017-03-15 10:32:07 +0100

    Màj

--- TimeProject/Models/request/Authentification.cs
+++ TimeProject/Models/request/Authentification.cs
@@ -18,7 +18,7 @@ namespace TimeProject.Models.request
             User user;
 
             DataBase.ConnexionToDataBase();
-            dataReader = DataBase.DBSelect("SELECT * FROM User WHERE NOM_USER = '" + identifiant + "' AND PWD = '" + mdp + "'");
+            dataReader = DataBase.DBSelect("SELECT * FROM User WHERE MAIL = '" + identifiant + "' AND PWD = '" + mdp + "'");
             if (dataReader.Read())
             {
                 user = new User(Convert.ToInt32(dataReader[0]), dataReader[1].ToString(), dataReader[2].ToString(), dataReader[3].ToString(), dataReader[4].ToString(), dataReader[5].ToString(), dataReader[6].ToString());
