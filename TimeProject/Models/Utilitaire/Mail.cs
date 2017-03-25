cb7a24d070bdcaf4389ce86dc2ba2dad94fd0fc6
Jakception <gbeulaygue@gmail.com>
2017-03-25 16:26:07 +0100

    Mail fait et tester : OK

--- TimeProject/Models/Utilitaire/Mail.cs
+++ TimeProject/Models/Utilitaire/Mail.cs
@@ -13,20 +13,21 @@ namespace TimeProject.Models.Utilitaire
 {
     public static class Mail
     {
-        public static void EnvoiMail(string sourceName, string emailRecepteur, string sujet, string contenuMail)
+        // AppCOnfig avec appSettings
+        public static void EnvoiMail(string emailRecepteur, string sujet, string contenuMail)
         {
-            Hashtable section = (Hashtable)ConfigurationManager.GetSection(sourceName);
+            var appSettings = ConfigurationManager.AppSettings;
 
-            var fromAddress = new MailAddress(section["email"].ToString(), "From Name");
+            var fromAddress = new MailAddress(ConfigurationManager.AppSettings["email"].ToString(), "From Name");
             var toAddress = new MailAddress(emailRecepteur, "To Name");
-            string fromPassword = section["mdp"].ToString();
+            string fromPassword = ConfigurationManager.AppSettings["mdp"].ToString();
             string subject = sujet;
             string body = contenuMail;
 
             var smtp = new SmtpClient
             {
-                Host = section["host"].ToString(),
-                Port = Convert.ToInt32(section["port"]),
+                Host = ConfigurationManager.AppSettings["host"].ToString(),
+                Port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]),
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
@@ -49,6 +50,44 @@ namespace TimeProject.Models.Utilitaire
                 }
             }
         }
+        // AppConfig avec les sections
+        //public static void EnvoiMail(string sourceName, string emailRecepteur, string sujet, string contenuMail)
+        //{
+        //    Hashtable section = (Hashtable)ConfigurationManager.GetSection(sourceName);
+
+        //    var fromAddress = new MailAddress(section["email"].ToString(), "From Name");
+        //    var toAddress = new MailAddress(emailRecepteur, "To Name");
+        //    string fromPassword = section["mdp"].ToString();
+        //    string subject = sujet;
+        //    string body = contenuMail;
+
+        //    var smtp = new SmtpClient
+        //    {
+        //        Host = section["host"].ToString(),
+        //        Port = Convert.ToInt32(section["port"]),
+        //        EnableSsl = true,
+        //        DeliveryMethod = SmtpDeliveryMethod.Network,
+        //        UseDefaultCredentials = false,
+        //        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+        //    };
+        //    using (var message = new MailMessage(fromAddress, toAddress)
+        //    {
+        //        Subject = subject,
+        //        Body = body
+        //    })
+        //    {
+        //        try
+        //        {
+        //            smtp.Send(message);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            System.Windows.Forms.MessageBox.Show("Exception caught in EnvoiMail(): {0}",
+        //                ex.ToString());
+        //        }
+        //    }
+        //}
+        // Référence de la méthode
         //public static void EnvoiMail(string source, string emailRecepteur, string sujet, string contenuMail)
         //{
         //    var fromAddress = new MailAddress("EMAIL@EXEMPLE.COM", "From Name");
