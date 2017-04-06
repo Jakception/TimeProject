77643300ce7af54b1ae1959e10536d979a41f497
Jakception <gbeulaygue@gmail.com>
2017-04-06 14:15:38 +0200

    Dossier rangement

--- TimeProject/Form/FormProjet.cs
+++ TimeProject/Form/FormProjet.cs
@@ -37,7 +37,7 @@ namespace TimeProject
 
         private void button3_Click(object sender, EventArgs e)
         {
-            f1.createBordereau();
+            //f1.createBordereau();
         }
 
         private void lblTitre_Click(object sender, EventArgs e)
